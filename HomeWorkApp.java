384f19161db61f11c95fe4f61fdae62f88624b91
viktor-kr <viktor.kr1984@yandex.ru>
2025-02-01 19:59:31 +0300

    add code lesson1

---
+++ HomeWorkApp.java
@@ -0,0 +1,55 @@
+public class HomeWorkApp {
+    public HomeWorkApp() {
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("| Orange |");
+        System.out.println("| Banana |");
+        System.out.println("| Apple  |");
+    }
+
+    public static void checkSumSign() {
+        int a = 100000;
+        int b = 5000000;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a = 19;
+        int b = 20;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+
+    }
+}
