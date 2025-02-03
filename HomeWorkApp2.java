5e4a2b1a058e1c8fff382dc77a9a1721339e47f5
viktor-kr <viktor.kr1984@yandex.ru>
2025-02-03 19:57:21 +0300

    add code homework lesson2

---
+++ HomeWorkApp2.java
@@ -0,0 +1,58 @@
+public class HomeWorkApp2 {
+    public static void main(String[] args) {
+
+        twoIntegers(9, 1);
+        checkNumber(-1);
+        numberSign(1);
+        setString("is not a bug", 5);
+        checkYear(1988);
+    }
+
+    public static boolean twoIntegers(int first, int second) {
+
+        if (first + second > 10 && first + second <= 20) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+
+    public static void checkNumber(int number) {
+        if (number >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean numberSign(int number) {
+        if (number < 0) {
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void setString(String myString, int counter) {
+        for (int i = 0; i < counter; i++) {
+            System.out.println(myString);
+        }
+    }
+
+    public static boolean checkYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+}
+
