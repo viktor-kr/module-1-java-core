c32b79426e8bb46f8b36ac9fe9633987a9bf0ac9
viktor-kr <viktor.kr1984@yandex.ru>
2025-02-17 19:57:15 +0300

    add fix2 code homework lesson5

--- Lesson5/Employee.java
+++ Lesson5/Employee.java
@@ -26,28 +26,9 @@ public class Employee {
     }
 
     public int emploeeAge() {
+
         return age;
     }
 
-    public static void main(String[] args) {
 
-
-        Employee [] emploeeInformation = new Employee[5];
-        emploeeInformation[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com",
-                "892312312", 30000, 33);
-
-        emploeeInformation[1] = new Employee("Erohof Oleg", "Senior Engineer", "olegn@mailbox.com",
-                "892312355", 90000, 50);
-        emploeeInformation[2] = new Employee("Petrov Andry", "QA", "petr@mailbox.com",
-                "892312366", 100000, 40);
-        emploeeInformation[3] = new Employee("Sidorov Kumar", "Project Manager", "kumar@mailbox.com",
-                "892312772", 130000, 55);
-        emploeeInformation[4] = new Employee("Gorelov Zakir", "CPO", "zakir@mailbox.com",
-                "892312777", 230000, 22);
-        for (int i = 0; i < emploeeInformation.length; i++) {
-            if (emploeeInformation[i].emploeeAge() > 40) {
-                System.out.println(emploeeInformation[i].emploeeInformation());
-            }
-        }
-    }
 }
